aa7ec4512d38e500baaef444ff26cebf101f43f8
leewind <leewind@leewinddeMacBook-Pro.local>
2017-05-20 17:23:02 +0800

    [dev]microservice try

---
+++ consumer/consumer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/leewind/git-project-collection/proto/collector"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewCollectorClient(conn)
+	r, err := c.GetStarredRepositories(context.Background(), &pb.Empty{})
+	if err != nil {
+		log.Fatalf("could not Collector: %v", err)
+	}
+	log.Printf("Collector: %s", r.Repos)
+}
